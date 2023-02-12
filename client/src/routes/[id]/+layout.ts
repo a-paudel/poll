3c892528d378aa93d209229b9f7b4d38a5c7a90c
apaudel96 <apaudel96@ymail.com>
2023-02-12 19:58:10 +0000

    Go to homepage if poll doesnt exist

--- client/src/routes/[id]/+layout.ts
+++ client/src/routes/[id]/+layout.ts
@@ -1,11 +1,21 @@
+import { goto } from "$app/navigation";
+import Swal from "sweetalert2";
 import { API } from "../../api";
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ params }) => {
   let id = parseInt(params.id);
-  let question = await API.getQuestion(id);
-
-  return {
-    question,
-  };
+  try {
+    let question = await API.getQuestion(id);
+    return {
+      question,
+    };
+  } catch (e) {
+    await goto("/");
+    await Swal.fire({
+      title: "Error",
+      text: "Poll not found",
+      icon: "error",
+    });
+  }
 };
