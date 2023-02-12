118b228f5e16727f558cc2513f1afdb1823de0b1
apaudel96 <apaudel96@ymail.com>
2023-02-12 19:53:10 +0000

    create database folder

--- server/models/main.go
+++ server/models/main.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"os"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -8,6 +10,9 @@ import (
 var DB *gorm.DB
 
 func init() {
+	// create database folder if not exists
+	err := os.MkdirAll("data/database", os.ModePerm)
+
 	db, err := gorm.Open(sqlite.Open("data/database/poll.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
